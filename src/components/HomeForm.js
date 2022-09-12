842bed403ab4a69fd73b38a0bf2634cc374e3e50
marvinorias <oriasmarvinm@gmail.com>
2022-09-13 03:23:09 +0800

    Notification at home page added

--- src/components/HomeForm.js
+++ src/components/HomeForm.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 
 export default function HomeForm(){
+	const ref = useRef(true)
+
+	useEffect(() => {
+		if(ref.current){
+			ref.current = false;
+			alert(`Users page has been changed to table format.
+				   Confirmation before removing a user.
+				   Validation added to age.
+				   Have a nice day!`);
+		}
+	}, []);
+
 	return(
 		<>
 			<div className="div-home">Welcome QMarketz!</div>
